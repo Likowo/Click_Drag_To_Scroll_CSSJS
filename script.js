d0fee1daf8cdf451632a03ed7d76c72dd24150ec
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-09 20:22:55 -0500

    Did some css to display slider with horizontal scroll

--- script.js
+++ script.js
@@ -6,21 +6,23 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', () => {
     isDown = true;
+    slider.classList.add('active'); // Pops up slides abit
 });
 
 slider.addEventListener('mouseup', () => {
     isDown = false;
+    slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseleave', () => {
     isDown = false;
+    slider.classList.remove('active');
 });
 
 slider.addEventListener('mousemove', () => {
-
-});
-
-slider.addEventListener('mousedown', () => {
+    if(!isDown){
+        return; // stops function from running
+    }
     console.log(isDown)
     console.log('Do work')
 });
