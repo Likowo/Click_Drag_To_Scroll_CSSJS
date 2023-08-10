109f8ba1eb2bce208389055f9278cc56b36356df
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-09 21:26:10 -0500

    added event listerner to mousedown and mousemove events.More css done

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // Select element from DOM
 const slider = document.querySelector('.slider');
 let isDown = false; // Our flag variable for when mouse moves
-let startx;
-let scrollLeft;
+let startx;     //Records initial clickdown on x-axis (i.e. horizontally)
+let scrollLeft;  // Records position of initial scroll
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
     isDown = true;
     slider.classList.add('active'); // Pops up slides abit
+    // console.log(e)
+    startx = e.pagex - slider.offsetleft; //Takes away any extra space between point of clickx and image of interest
+    scrollLeft = slider.scrollLeft;
+
 });
 
 slider.addEventListener('mouseup', () => {
@@ -22,9 +26,13 @@ slider.addEventListener('mouseleave', () => {
 slider.addEventListener('mousemove', () => {
     if(!isDown){
         return; // stops function from running
-    }
-    console.log(isDown)
-    console.log('Do work')
+    };
+    e.preventDefault(); //Prevents selecting of text within slides
+    const x = e.pagex - slider.offsetleft; // Gives position of cursor when moved from left to right and vice versa
+    const movement = x - startx;
+    slider.scrollLeft = scrollLeft - movement;
+    console.log(isDown);
+    console.log(x,startx);
 });
 
 
